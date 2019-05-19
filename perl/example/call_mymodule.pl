e714462067d3defa33a17ee05febb3a4b18f593e
ACT3 <chuiC456@163.com>
2019-05-19 13:45:36 -0400

    Fix call mymodule failed.

--- perl/example/call_mymodule.pl
+++ perl/example/call_mymodule.pl
@@ -1,5 +1,11 @@
 #!/usr/bin/perl
 
-use mymodule; # import the module
+use File::Basename qw(dirname);
+use Cwd qw(abs_path);
+use lib dirname(abs_path($0)); # add the current dir to @INC
 
-print mymodule::f1(5); # call the function
+use Data::Dumper;
+print Dumper(\@INC);
+
+use mymodule; # import the module
+print mymodule::f1(5), "\n"; # call the function
